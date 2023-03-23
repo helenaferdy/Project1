afbad5a1f96385d1f446a2608003e1ee33fd1bdf
Sena <36813961+helenaferdy@users.noreply.github.com>
2023-03-23 19:47:53 +0700

    fix env csv

--- lib/getEnvi/main.py
+++ lib/getEnvi/main.py
@@ -10,12 +10,6 @@ def getEnvi():
         csv_reader = csv.DictReader(csv_file)
         data = [row for row in csv_reader]
 
-
-    # print('''
-    # Welcome to helena network automation xxz
-    # These are your devices : 
-    # ''')
-        
     devices = []
     for d in data:
         print(f"{d['hostname']} : {d['ip']}")
@@ -24,22 +18,21 @@ def getEnvi():
             d['ip'],
             d['username'],
             d['password'],
-            d['enable']
+            d['enable_password']
         )
         devices.append(new_router)
 
-    commands_x = "show environment"
+    command = "show environment"
     headers = ['Hostname', 'Site', 'Power Supply', 'Temperature', 'Fan']
-    try:
-        with open(f"out/Environment/{commands_x}_summary.csv", mode="w", newline="") as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(headers)
-    except:
-        pass
+ 
+    with open(f"out/Environment/{command}_summary.csv", mode="w", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(headers)
+
 
     threads = []
     for device in devices:
-        thread = threading.Thread(target=device.connect, args=(commands_x,))
+        thread = threading.Thread(target=device.connect, args=(command,))
         thread.start()
         threads.append(thread)
 
